f43ffbe0fe7e3450f5ab1537cd4eb83c265ab9d7
root <root@ip-172-31-14-240.sa-east-1.compute.internal>
2025-04-05 01:19:59 +0000

    feat: autenticação jwt

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from bs4 import BeautifulSoup
 import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import (
-    JWTManager, create_acess_token,
+    JWTManager, create_access_token,
     jwt_required, get_jwt_identity
 )
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
@@ -37,6 +38,78 @@ auth = HTTPBasicAuth()
 @app.route('/')
 def home():
     return "Hello, flask"
+
+
+@app.route("/register", methods=['POST'])
+def registrer_user():
+    """
+    Registar um novo usuario
+    ---
+    parametros:
+        - in: body
+          name: body
+          required: true
+          schema:
+            type: object
+            properties:
+                username:
+                    type: string
+                password:
+                    type: string
+    responses:
+        201:
+            description: Usuario criado com sucesso
+        400:
+            description: Usario já existe
+    """
+
+    data = request.get_json()
+    if User.query.filter_by(username=data['username']).first():
+        return jsonify({'error': 'User already exists'}), 400
+    new_user = User(username=data['username'], password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'msg': 'user created'}), 201
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    """
+    Fazer login do usuario e retornar um jwt
+    ---
+    parametros:
+        - in: body
+          name: body
+          required: true
+          schema:
+            type: object
+            properties:
+                username:
+                    type: string
+                password:
+                    type: string
+    responses:
+        201:
+            description: Login bem sucedido, retorna JWT
+        400:
+            description: Credenciais inválidas
+    """
+
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.password == data['password']:
+        # convert o ID em uma string
+        token = create_access_token(identity=str(user.id))
+        return jsonify({'access_token': token}), 200
+    return jsonify({'error': 'Invalid credentials'}), 401
+
+
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    # Retona o 'identity' usado na criação do token
+    current_user_id = get_jwt_identity()
+    return jsonify({'msg': f'Usuario com ID {current_user_id} acessou a rota protegida'}), 200
 
 
 if __name__ == '__main__':
